ac2a7979fa871bc179f61ab80e7b2f19c4f8684a
baloghd <balogh.domonkos@gmail.com>
2018-11-19 20:08:12 +0100

    Matrix_muvelet

--- Matrix_muvelet.c
+++ Matrix_muvelet.c
@@ -8,7 +8,7 @@
 #include "Matrix_muvelet.h"
 
 
-/* megszorozza egy sor összes elemét egy számmal, új sort visszaadva */
+/** megszorozza egy sor összes elemét egy számmal, új sort visszaadva **/
 double *sorszoroz(double *sor, int meret, double mivel)
 {
     double *vissza = (double *) malloc(meret * sizeof(double));
@@ -18,7 +18,7 @@ double *sorszoroz(double *sor, int meret, double mivel)
     }
     return vissza;
 }
-/* megszorozza egy paraméterként kapott sor összes elemét egy számmal*/
+/** megszorozza egy paraméterként kapott sor összes elemét egy számmal**/
 void sorszoroz_helyben(double *sor, int meret, double mivel)
 {
     for (int i = 0; i < meret; ++i)
@@ -26,7 +26,7 @@ void sorszoroz_helyben(double *sor, int meret, double mivel)
         sor[i] *= mivel;
     }
 }
-/* összeadja egy paraméterként kapott sor összes elemét egy számmal*/
+/** összeadja egy paraméterként kapott sor összes elemét egy számmal**/
 double *sorosszead(double *sor1, double *sor2, int meret)
 {
     double *vissza = (double *) malloc(meret * sizeof(double));
@@ -36,7 +36,7 @@ double *sorosszead(double *sor1, double *sor2, int meret)
     }
     return vissza;
 }
-/* összeadja egy paraméterként kapott sor összes elemét egy számmal*/
+/** összeadja egy paraméterként kapott sor összes elemét egy számmal**/
 void sorosszead_helyben(double *sor1, double *sor2, int meret)
 {
     for (int i = 0; i < meret; ++i)
@@ -44,7 +44,7 @@ void sorosszead_helyben(double *sor1, double *sor2, int meret)
         sor1[i] += sor2[i];
     }
 }
-/** http://mathworld.wolfram.com/MatrixTrace.html **/
+/*** http://mathworld.wolfram.com/MatrixTrace.html ***/
 double Matrix_nyom(Matrix m)
 {
     assert(m->sor == m->oszlop);
@@ -55,14 +55,14 @@ double Matrix_nyom(Matrix m)
     }
     return nyom;
 }
-/* megcseréli egy paraméterként kapott mátrix megadott sorait (0-tól indexelve) */
+/** megcseréli egy paraméterként kapott mátrix megadott sorait (0-tól indexelve) **/
 void Matrix_sorcsere_helyben(Matrix *m, int egyiksor, int masiksor)
 {
     double *tarolo = m->tomb[egyiksor];
     m->tomb[egyiksor] = m->tomb[masiksor];
     m->tomb[masiksor] = tarolo;
 }
-/* megcseréli egy paraméterként kapott mátrix megadott oszlopait (0-tól indexelve) */
+/** megcseréli egy paraméterként kapott mátrix megadott oszlopait (0-tól indexelve) **/
 void Matrix_oszlopcsere_helyben(Matrix *m, int egyikoszlop, int masikoszlop)
 {
     for (int i = 0; i < m->sor; ++i)
@@ -72,7 +72,7 @@ void Matrix_oszlopcsere_helyben(Matrix *m, int egyikoszlop, int masikoszlop)
         m->tomb[i][masikoszlop] = tarolo;
     }
 }
-/* összead két mátrixot */
+/** összead két mátrixot **/
 Matrix Matrix_osszead(Matrix m1, Matrix m2)
 {
     assert(((m1.sor == m2.sor) && (m2.oszlop == m2.oszlop)) && "HIBA: csak azonos meretu Matrixok adhatoak ossze.");
@@ -89,7 +89,7 @@ Matrix Matrix_osszead(Matrix m1, Matrix m2)
 
     return t;
 }
-/* megszoroz két mátrixot */
+/** megszoroz két mátrixot **/
 Matrix Matrix_szorzas(Matrix jobb, Matrix bal)
 {
     assert(((jobb.sor == bal.oszlop) && (jobb.oszlop == bal.sor)) && "HIBA: szorzando Matrixok dimenzioira igaznak kell lennie, hogy [i x j] * [j x i]");
@@ -110,7 +110,7 @@ Matrix Matrix_szorzas(Matrix jobb, Matrix bal)
     }
     return szorzat;
 }
-/* segédfüggvény a Gauss-eliminációhoz */
+/** segédfüggvény a Gauss-eliminációhoz **/
 int _van_meg_nemnulla_sor(Matrix m, int kezdosor)
 {
     for (int i = kezdosor + 1; i < m.sor; ++i)
@@ -120,7 +120,7 @@ int _van_meg_nemnulla_sor(Matrix m, int kezdosor)
     }
     return -1;
 }
-/** a Gauss elimináció még nem 100%-osan működő **/
+/*** a Gauss elimináció még nem 100%-osan működő ***/
 void Matrix_Gauss(Matrix *m)
 {
     int k = m->sor;
